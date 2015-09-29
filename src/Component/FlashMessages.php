da3a5fdb2b55d17d272f55385b9c4f8393606cbe
David Matejka <matej21@matej21.cz>
2015-09-29 23:36:14 +0200

    FlashMessages component: fixed error class + refactoring

--- src/Component/FlashMessages.php
+++ src/Component/FlashMessages.php
@@ -13,16 +13,28 @@ class FlashMessages extends Control
 {
 
 	public static $typeClasses = [
-		FlashMessage::TYPE_INFO    => 'alert alert-info',
+		FlashMessage::TYPE_INFO => 'alert alert-info',
 		FlashMessage::TYPE_SUCCESS => 'alert alert-success',
 		FlashMessage::TYPE_WARNING => 'alert alert-warning',
-		FlashMessage::TYPE_ERROR   => 'alert alert-error',
-		NULL                       => 'alert',
+		FlashMessage::TYPE_ERROR => 'alert alert-danger',
+		NULL => 'alert',
 	];
+
+	/** @var array|null */
+	private $typeClassMapping;
 
 
 	public function __construct()
 	{
+	}
+
+
+	/**
+	 * @param array
+	 */
+	public function setTypeClassMapping(array $typeClassMapping)
+	{
+		$this->typeClassMapping = $typeClassMapping;
 	}
 
 
@@ -37,7 +49,7 @@ class FlashMessages extends Control
 
 	public function render()
 	{
-		$this->template->typeClasses = self::$typeClasses;
+		$this->template->typeClasses = $this->typeClassMapping ?: self::$typeClasses;
 		$this->template->flashes = $this->parent->template->flashes;
 		if (!$this->template->getFile()) {
 			$this->template->setFile(__DIR__ . '/flashMessages.latte');
